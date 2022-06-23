e71716131355e9b62f106f2450c822bcef36d4dd
Vicente Garcia <vicen1@hotmail.com>
2022-06-23 16:21:25 -0500

    Implement sign up and login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
-
+  const [login, { error }] = useMutation(LOGIN_USER);
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -16,12 +18,14 @@ const Login = (props) => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -53,6 +57,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
